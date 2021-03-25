d73e76d260d850a2a91765447ab88e7c2b32f778
Naveen Varma <naveenv802@gmail.com>
2021-03-25 00:48:09 -0400

    Added components

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,13 @@
+import PageContent from "../pageContent/PageContent";
+import '../home/Home.css'
+
+function Profile(){
+    return(
+        <div className="HomeContent">
+            <PageContent title="Profile" content="This is from profile page" />
+        </div>
+         
+    )
+}
+
+export default Profile;
---
+++ src/components/project/Project.js
@@ -0,0 +1,13 @@
+import PageContent from "../pageContent/PageContent"
+import '../home/Home.css'
+
+function Project(){
+    return(
+        <div className="HomeContent">
+            <PageContent title="Project" content="This is from project page" />
+        </div>
+        
+    )
+}
+
+export default Project
