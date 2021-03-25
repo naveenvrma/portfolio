7ec6b7ceed9248c51d38a7e3cb8b620471da389d
Naveen Varma <naveenv802@gmail.com>
2021-03-25 00:42:33 -0400

    Created a portfolio using react - created header and page content components - used react-router for routing - added css styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import PageContent from './components/pageContent/PageContent';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Profile from './components/profile/Profile';
+import Project from './components/project/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/profile" component={Profile}></Route>
+          <Route path="/project" component={Project}></Route>
+        </Switch>
+      </div>
+    
+    </Router>
+    
   );
 }
 
